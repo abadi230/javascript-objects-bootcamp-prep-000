3c65d0959493b9b7451b25f6f27579df8d03d6ce
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-25 12:15:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,19 +1,4 @@
-/*var playlist = {
-  artist: 'song pair'
-};
 
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = songTitle;
-  return playlist;
-  // return Object.assign({}, playlist, {[artistName]: songTitle})
-}
-
-function removeFromPlaylist(playlist, artistName){
-  delete playlist.Slowdive;
-  // return playlist
-}
-
-*/
 const playlist = {'Talal Maddah': 'A7ebak lo'}
 function updatePlaylist(obj, key, value){
   obj[key]= value;
